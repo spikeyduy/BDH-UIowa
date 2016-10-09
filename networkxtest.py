a29fb770d5d5ea3816913a3efa33eed8517d8d3b
Spikes Phan <spikeyduy@gmail.com>
2016-10-09 13:11:01 -0500

    Add files via upload
    Added a year label to the edge. Added a visual of who the player went through to reach the end player. Still can't fix the run time problem of query that amount of data and to find the shortest path. Am working on Breadth First Search. Need to add user input through Flask so user can pick their NFL players. Also need to set up Flask.

--- networkxtest.py
+++ networkxtest.py
@@ -12,18 +12,13 @@ teamList = open('names.txt','r')
 teamroster = open('teamRosters.txt','r')
 
 def getNodes():
-    for name in teamList.readlines():
+    for name in teamList.readlines()[:1000]:    #fix this, too long
+        # too long: takes too much time to query through
+        # and will take too long to go anything above 1000-2000
         #  print(name.strip())
          G.add_node(name.strip())
          #print(G.number_of_nodes())
     teamList.close()
-
-def add_attribute_to_edge(G,id_node_source,id_node_target,new_attr,value_attr):
-        keydict=G[id_node_source][id_node_target]
-        key=len(keydict)
-        for k in keydict:
-            if 'teamer' not in G.edge[id_node_source][id_node_target][k]:
-                    G.add_edge(id_node_source,id_node_target,key=k,new_attr=value_attr)
 
 def createRosters():
 # create for each team, a set (no-repeat dict) 
@@ -40,18 +35,8 @@ def createRosters():
             if len(roster[key])>1:    # if there is more than 1 data piece
                 #print(roster[key])
                 for i in range(len(roster[key])-1):
-                    # for j in range(len(roster[key])):
-                        # duplicate edges?
-                        # set up them into sets?
-                        # G.add_edge(roster[key][i],roster[key][j])
-                    # G.add_edges_from(roster[key])
-                    # could use add_path to create an auto path between them
-                    #G.add_path(roster[key])     # creates too many duplicate edges
-                      # if the node is in the graph, THEN do this
                     if G.has_node(roster[key][i]) & G.has_node(roster[key][i+1]):
-                        G.add_edge(roster[key][i],roster[key][i+1],teamer=22,yearer=33)
-                        # NEED TO CHANGE TEAMER AND YEARER
-                        # adding the team value to the edge and maybe the year value?
+                        G.add_edge(roster[key][i],roster[key][i+1],year=key)    # makes the edge have the common year
         # print(y)
         #print(roster)
     teamroster.close()
@@ -66,35 +51,57 @@ def node_colors(G,path):
     return colors
 
 def draw_shortest_path(G,pos,start,end):
-        path = nx.shortest_path(G,start,end)    # may need another param
-        size = len(path)
+        # have to fix this. Goes through EVERY PATH to find shortest
+        path = nx.shortest_path(G,start,end)    
+        size = len(path)-1  # how many degrees away
         colors = node_colors(G,path)
         edge_labels={(n1,n2):G[n1][n2] for (n1,n2) in G.edges()}
-        pylab.figure(1,figsize=(8,8))
-        nx.draw_networkx(G,pos,node_color=colors)
+        pylab.figure(1,figsize=(500,500))   # size of plot
+        nx.draw_networkx(G,pos,node_color=colors)  
         nx.draw_networkx_edge_labels(G,pos,edge_labels=edge_labels)
         if size > 1:
             print(start+' is '+str(size)+' degrees away from '+end)
+            for i in range(size):
+                # should we do a forloop where we just say 'he is connected to this guy'
+                print(str(path[i]) + ' >>> '+ str(path[i+1]))
         else:
             print(start+' is '+str(size)+' degree away from '+end)
 
-def sixDegrees(firstplaya, secondplaya):
-    # easy way, recursive? have to check the weights of the edges
-    # need to put weights on the edges first
-    print('nothing')
+def draw_bfs_path(G,pos,start,end):
+        path=nx.bfs_tree(G,start)
+        size = 0
+        queue=[]
+        queue.append([start])
+        while queue:
+            pather = queue.pop(0)
+            node = pather[-1]
+            if node == end:
+                print(pather)
+            for adjacent in path:
+                new_pather = list(path)
+                new_pather.append(adjacent)
+                queue.append(new_pather)
+            # print(queue)
 
-def drawthisshit():
+        colors = node_colors(G,path)
+        edge_labels={(n1,n2):G[n1][n2] for (n1,n2) in G.edges()}
+        pylab.figure(1,figsize=(500,500))
+        nx.draw_networkx(G,pos,node_color=colors)
+        nx.draw_networkx_edge_labels(G,pos,edge_labels=edge_labels)
+
+def drawthis():
     # nx.draw(G, node_size = 1, node_color = 'cyan')
     nx.draw(G,pos)
     #nx.draw_random(G)
-    print(G.number_of_nodes())
-    print(G.number_of_edges())
+    #print(G.number_of_nodes())
+    #print(G.number_of_edges())
 
 
 getNodes()
 createRosters()
 pos = graphviz_layout(G,prog='neato')
-#drawthisshit()
-draw_shortest_path(G,pos,'Bryan Anderson','Jerry Azumah')
-#pylab.show()
+drawthis()
+draw_shortest_path(G,pos,'Donnie Abraham','Cliff Aberson')
+#draw_bfs_path(G,pos,'Donnie Abraham','Cliff Aberson')
+pylab.show()
 #plt.savefig("testforNet", dpi=1000)
